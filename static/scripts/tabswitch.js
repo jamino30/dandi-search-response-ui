3a1eff83e5e738968d5427f2c9ffc9636331cead
jai <jaiamin130@gmail.com>
2023-10-26 11:55:26 -0400

    Update button format for dandiset views

--- static/scripts/tabswitch.js
+++ static/scripts/tabswitch.js
@@ -2,27 +2,15 @@
 function showTab(tabIdx) {
     var tabs = document.querySelectorAll('.tab');
     var startTab = document.querySelector('.start-tab');
-    var tabButtons = document.querySelectorAll('.tabButton');
     
-    var activeTab;
     for (var i = 0; i < tabs.length; i++) {
         tabs[i].style.display = "none";
-        if (tabButtons[i].classList.contains('active')) {
-            activeTab = tabs[i]
-        }
-        tabButtons[i].classList.remove('active');
     }
-
-    if (activeTab == tabs[tabIdx] && tabIdx != -1) {
-        startTab.style.display = "block";
-    }
-    else {
-        if (tabIdx != -1) {
-            tabs[tabIdx].style.display = "block";
-            tabButtons[tabIdx].classList.add('active');
-            startTab.style.display = "none";
-        } 
-    }
+    
+    if (tabIdx != -1) {
+        tabs[tabIdx].style.display = "block";
+        startTab.style.display = "none";
+    } 
 };
 
 // show the initial start tab
