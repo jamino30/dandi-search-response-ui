79ce44fbe89c9eabaf04a15f94ea8df97ea9e489
jai <jaiamin130@gmail.com>
2023-10-27 15:47:49 -0400

    Add custom object key algorithm

--- rest/app.py
+++ rest/app.py
@@ -37,18 +37,31 @@ async def submit_data(responses: ResponseItem):
 
     s3 = boto3.client("s3")
     bucket_name = "dandi-search-bucket"
-    object_key = f"response_data_{uuid.uuid4()}.json"
 
     try:
         s3.put_object(
             Bucket=bucket_name, 
-            Key=object_key, 
+            Key=generate_object_key(data["query"]), 
             Body=json_data,
             ContentType="application/json"
         )
         return {"message": "Responses uploaded"}
     except Exception as e:
         return {"error": str(e)}
+    
+def generate_object_key(user_query: str, max_length=1024):
+    unique_id = str(uuid.uuid4())
+    type = ".json"
+    remaining = max_length - len(type) - len(unique_id)
+
+    formatted_query = "_".join(user_query.strip().lower().split())
+    if len(formatted_query) > remaining:
+        formatted_query = formatted_query[:remaining]
+        if len(formatted_query) > 0 and formatted_query[-1] == "_":
+            formatted_query = formatted_query[:-1]
+
+    object_key = f"{formatted_query}_{unique_id}{type}"
+    return object_key
 
 
 # read styling and script files
