b8ac657367f40b0554bdf3c988c63d942149be0d
jai <jaiamin130@gmail.com>
2023-11-08 10:25:46 -0500

    Switch from async to sync for fastapi threadpool

--- rest/app.py
+++ rest/app.py
@@ -19,7 +19,7 @@ class ResponseItem(BaseModel):
 
 
 @app.post("/scan/")
-async def scan_query(query_item: QueryItem):
+def scan_query(query_item: QueryItem):
     try:
         result: dict = scan_for_relevant_dandisets(query_item.query)
         return result
@@ -27,11 +27,11 @@ async def scan_query(query_item: QueryItem):
         return {"error": str(e)}, 500
 
 @app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
+def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/upload/")
-async def submit_data(responses: ResponseItem):
+def submit_data(responses: ResponseItem):
     s3_client = S3Bucket("dandi-search-llm-bucket")
 
     try:
@@ -43,9 +43,9 @@ async def submit_data(responses: ResponseItem):
 
 # read styling and script files
 @app.get("/styles.css", response_class=FileResponse)
-async def read_css():
+def read_css():
     return "static/styles.css"
 
 @app.get("/scripts/{script_name}", response_class=FileResponse)
-async def read_script(script_name: str):
+def read_script(script_name: str):
     return f"static/scripts/{script_name}"
